956dfb35dc52011a193d56d358f772525c6e4b0a
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-19 22:04:04 -0700

    Update odin-warfather.user.js

--- odin-warfather.user.js
+++ odin-warfather.user.js
@@ -5,6 +5,9 @@
 // @description  Faction War Engine
 // @author       BjornOdinsson89
 // @match        https://www.torn.com/*
+// @match        https://www.torn.com/*?*
+// @match        https://www.torn.com/*#*
+// @match        https://*.torn.com/*
 // @updateURL    https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather.user.js
 // @downloadURL  https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather.user.js
 // @grant        GM_addStyle
@@ -24,11 +27,33 @@
 // @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/war-tab.js
 // @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/chain-tab.js
 // @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/targets-tab.js
-// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/console-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/consyole-tab.js
 // @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/odin-gui-integrator.js
 // ==/UserScript==
 
 (function() {
     const css = GM_getResourceText("odinCSS");
     GM_addStyle(css);
+
+    const init = () => {
+        if (!window.OdinWarGUI || !window.WarfatherGUI) {
+            setTimeout(init, 120);
+            return;
+        }
+
+        const userID = parseInt(
+            document.querySelector("a[href*='XID=']")?.href?.match(/XID=(\d+)/)?.[1]
+        ) || 0;
+
+        const factionID =
+            window?.wfFactionID ||
+            GM_getValue("wfFactionID") ||
+            0;
+
+        window.WF_STATE = { userID, factionID };
+
+        window.WarfatherGUI.init();
+    };
+
+    init();
 })();
