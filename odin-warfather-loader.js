d3c275afbbeb8350084bf44585f7c27c8547f76d
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-19 20:31:16 -0700

    Create odin-warfather-loader.js
    Initial upload

---
+++ odin-warfather-loader.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         ODIN WARFATHER (Loader)
+// @namespace    https://github.com/bjornodinsson89/odin-warfather
+// @version      1.0.0
+// @description  Auto-updater loader for WarFather
+// @author       BjornOdinsson89
+// @match        https://www.torn.com/*
+// @updateURL    https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather-loader.user.js
+// @downloadURL  https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather-loader.user.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather.user.js
+// ==/UserScript==
