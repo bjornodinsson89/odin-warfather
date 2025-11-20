5ee6621c20c054e2b83ef6decb24b7f75271cfbc
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-19 20:30:31 -0700

    Create odin-warfather.user.js
    Initial upload

---
+++ odin-warfather.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         ODIN WARFATHER
+// @namespace    https://github.com/bjornodinsson89/odin-warfather
+// @version      1.0.0
+// @description  Faction War Engine
+// @author       BjornOdinsson89
+// @match        https://www.torn.com/*
+// @updateURL    https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather.user.js
+// @downloadURL  https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/odin-warfather.user.js
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @run-at       document-end
+// @resource     odinCSS https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/odin-warfather.css
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/engines/sync-engine.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/engines/smartapi.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/engines/faction-engine.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/odin-gui.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/dashboard-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/faction-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/war-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/chain-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/targets-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/console-tab.js
+// @require      https://raw.githubusercontent.com/bjornodinsson89/odin-warfather/main/src/gui/odin-gui-integrator.js
+// ==/UserScript==
+
+(function() {
+    const css = GM_getResourceText("odinCSS");
+    GM_addStyle(css);
+})();
