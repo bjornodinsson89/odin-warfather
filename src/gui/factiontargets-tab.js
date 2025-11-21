02eb4fb3bce111d6cc74dbddf6bdd20f0e722cb5
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-20 22:22:51 -0700

    Create factiontargets-tab.js

---
+++ src/gui/factiontargets-tab.js
@@ -0,0 +1,6 @@
+export class WarfatherFactionTargetsTab {
+    constructor() {
+        const pane = document.getElementById("wf-tab-factionTargets");
+        pane.innerHTML = `<h2>Faction Targets</h2><p>Shared faction targets will load here.</p>`;
+    }
+}
