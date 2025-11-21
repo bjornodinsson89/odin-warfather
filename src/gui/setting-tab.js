ed21fcf0326162cbafcd34a0f8338c716b8caf97
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-20 22:22:17 -0700

    Create setting-tab.js

---
+++ src/gui/setting-tab.js
@@ -0,0 +1,6 @@
+export class WarfatherSettingsTab {
+    constructor() {
+        const pane = document.getElementById("wf-tab-settings");
+        pane.innerHTML = `<h2>Settings</h2><p>Settings go here.</p>`;
+    }
+}
